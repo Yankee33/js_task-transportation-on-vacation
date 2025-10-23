42260c4c01e293a0dd329542415bda2b83ef8d57
Yankee <pavlisdenis3@gmail.com>
2025-10-23 18:35:48 +0300

    create function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const basePrice = 40;
+  const longTerm = 7;
+  const longTermMoney = 50;
+  const shortTerm = 20;
+
+  if (days >= longTerm) {
+    return basePrice * days - longTermMoney;
+  }
+
+  if (days >= 3 && days <= 6) {
+    return basePrice * days - shortTerm;
+  }
+
+  if (days <= 2) {
+    return basePrice * days;
+  }
 }
 
 module.exports = calculateRentalCost;
